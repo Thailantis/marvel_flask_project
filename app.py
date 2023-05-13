18305ffc152f0f0070c1b70df08edaa60ff142b0
Thailantis <128191487+Thailantis@users.noreply.github.com>
2023-05-13 01:49:27 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_wtf.csrf import CSRFProtect
 from marvel_api import app, routes
 
 from routes import *
@@ -13,6 +14,7 @@ db - SQLAlchemy(app)
 migrate = Migrate(app, db)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
+csrf - CSRFProtect(app)
 
 if __name__ == '__main__':
     db.create_all()
