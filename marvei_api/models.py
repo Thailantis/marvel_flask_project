3b7568737f6bde00804dd5aabdacdf67e7bd57e5
Thailantis <128191487+Thailantis@users.noreply.github.com>
2023-05-13 21:15:49 -0400

    Update models.py

--- marvei_api/models.py
+++ marvei_api/models.py
@@ -1,6 +1,8 @@
 from app import app, db
 from datetime import datetine
-from flask_login import USerMixin
+from flask_login import USerMixin, LoginManager, login_user, login_required, logout_user, current_user
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField
 
 class User(db.Model, UserMixin):
   id = db.Column(db.String(40), primary_key=True)
@@ -19,3 +21,17 @@ class Character(db.Model):
   super_power = db.Column(db.String(200), nullable=False)
   date_created = db.Column(db.DateTime, default=datetime.utcnow)
   owner_id = db.Column(db.String(40), db.ForeignKey('user.id'), nullable=False)
+
+class LoginForm(FlaskForm):
+  email = StringField('Email', validators=[DataRequired(), Email()])
+  password = PasswordField('Password', validators=[DataRequired()])
+  
+class CharacterForm(FlaskForm):
+  name = StringField('Name', validators=[DataRequired()])
+  description = StringField('Description')
+  comics_appeared_in = StringField('Comics Appeared In')
+  super_power = StringField('Super Power')
+  
+@login_manager.user_loader
+def load_user(user_id):
+  return User.query.get(int(user_id))
